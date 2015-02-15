2e7e6ae12b1a5543e5f758052f7f04be042ef021
Anatol Belski <ab@php.net>
2015-02-15 17:56:49 +0000

    basic PHP7 compatibility
    git-svn-id: http://svn.php.net/repository/pecl/geoip/trunk@335948 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_geoip.h
+++ php_geoip.h
@@ -26,6 +26,10 @@ extern zend_module_entry geoip_module_entry;
 
 #define PHP_GEOIP_VERSION "1.1.1-dev"
 
+#if PHP_MAJOR_VERSION < 7
+typedef long zend_long;
+#endif
+
 #ifdef PHP_WIN32
 #define PHP_GEOIP_API __declspec(dllexport)
 #else
