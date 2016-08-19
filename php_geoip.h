1be8c8ef0f0f0478dc6f6e7a1fba3d9ebadc8b3b
Olivier Hill <ohill@php.net>
2016-08-19 00:36:36 +0000

    Going back to dev
    git-svn-id: http://svn.php.net/repository/pecl/geoip/trunk@339907 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_geoip.h
+++ php_geoip.h
@@ -24,7 +24,7 @@
 extern zend_module_entry geoip_module_entry;
 #define phpext_geoip_ptr &geoip_module_entry
 
-#define PHP_GEOIP_VERSION "1.1.1"
+#define PHP_GEOIP_VERSION "1.1.2-dev"
 
 #if PHP_MAJOR_VERSION < 7
 typedef long zend_long;
